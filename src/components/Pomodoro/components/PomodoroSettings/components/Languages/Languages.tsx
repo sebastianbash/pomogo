6db86313d97d06340e5cc550774aade40b7a02df
Sebastian <sebastian.silva.agurto@hotmail.com>
2023-01-26 08:44:30 -0500

    feat: :sparkles: a new settings components added for changing languages

---
+++ src/components/Pomodoro/components/PomodoroSettings/components/Languages/Languages.tsx
@@ -0,0 +1,29 @@
+import { handleChangeLng } from "@/utils";
+import { withTranslation } from "react-i18next";
+import { MdTranslate } from "react-icons/md";
+import { LanguagesInterface } from "../interfaces";
+
+const Languages: React.FC<LanguagesInterface> = ({ t }) => {
+  const lngLanguages: string = "pomodoro.settings.modal.body.languages";
+
+  return (
+    <div className="border-t-2">
+      <div className="flex items-center">
+        <h2 className="font-bold">{t(`${lngLanguages}.label`)}</h2>
+        <span className="text-xl ml-4">
+          <MdTranslate />
+        </span>{" "}
+      </div>
+      <div className="space-x-4">
+        <button className="text-lg" onClick={() => handleChangeLng("en")}>
+          {t(`${lngLanguages}.english`)}
+        </button>
+        <button className="text-lg" onClick={() => handleChangeLng("es")}>
+          {t(`${lngLanguages}.spanish`)}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default withTranslation()(Languages);
